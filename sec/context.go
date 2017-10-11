9a6629fd5887354267d7dbe9aeb26908a148d266
CaptpBdcht <get.clidouh@gmail.com>
2017-10-11 16:17:49 +0200

    Added password constraints

--- sec/context.go
+++ sec/context.go
@@ -7,11 +7,12 @@ import (
 // SecurityContext holding database connection and stuff.
 type SecurityContext struct {
 	connection *sql.DB
+	pwdConstraints *PasswordConstraints
 }
 
 // ContextInit is used to initialize the database connection and create the necessary
 // tables, storing a pointer of the database connection in the returned context.
-func ContextInit(dbPath string) (*SecurityContext, error) {
+func ContextInit(dbPath string, constraints *PasswordConstraints) (*SecurityContext, error) {
 	db, err := databaseOpen(dbPath)
 	if err != nil {
 		return nil, err
@@ -24,5 +25,16 @@ func ContextInit(dbPath string) (*SecurityContext, error) {
 	context := new(SecurityContext)
 	context.connection = db
 
+	initPwdConstraints(context, constraints)
+
 	return context, nil
+}
+
+func initPwdConstraints(context *SecurityContext, constraints *PasswordConstraints) {
+	
+	if constraints == nil {
+		context.pwdConstraints = NewDefaultPasswordConstraints()
+	} else {
+		context.pwdConstraints = constraints
+	}
 }
