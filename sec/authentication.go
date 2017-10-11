3ce7d6e49cc24be24097bde7f7627d172c878119
Mathieu Boisnard <m.boisnard@free.fr>
2017-10-11 15:58:11 +0200

    Register a new user in database

--- sec/authentication.go
+++ sec/authentication.go
@@ -8,18 +8,36 @@ TODO :
 
 package sec
 
-func register(context *SecurityContext, username string, password string) {
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// RegisterUser is the main function to add new user in database
+func RegisterUser(context *SecurityContext, username string, password string) (int64, error) {
+
+	newUserStatement, err := context.connection.Prepare("INSERT INTO users(username, password) values(?, ?)")
+	if err != nil {
+		return -1, err
+	}
+
+	res, err := newUserStatement.Exec(username, password)
+	if err != nil {
+		return -1, err
+	}
+
+	newID, err := res.LastInsertId()
+	if err != nil {
+		return -1, err
+	}
+
+	return newID, nil
+}
+
+// Login check username and password in database
+func Login(context *SecurityContext, username string, challengeResponse string) {
 
 }
 
-func login(context *SecurityContext, username string, challengeResponse string) {
-
-}
-
-func getPasswordStrength(password string) int {
-	return 0
-}
-
-func getChallenge() {
+func GetChallenge() {
 
 }
